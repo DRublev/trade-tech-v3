7e0d4e0943b2f68e6b35311c4cd432b49b3945e0
alexperminov <alexperminov94@gmail.com>
2024-02-21 22:14:21 +0400

    working

--- client/webpack.renderer.config.ts
+++ client/webpack.renderer.config.ts
@@ -5,16 +5,9 @@ import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
 rules.push({
-  // exclude: /(node_modules|\.webpack|\@radix\-ui)/,
-  exclude: path.resolve('./node_modules/'),
   test: /\.css$/,
   use: [{ loader: 'style-loader' }, {
     loader: 'css-loader', 
-    options: {
-      importLoaders: 1,
-      modules: true,
-      // localIdentName: "[name]__[local]___[hash:base64:5]"
-    },
   }],
 });
 
