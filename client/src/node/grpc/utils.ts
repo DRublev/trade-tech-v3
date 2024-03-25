1ca4f59cbeeea2b7c17012ffb68f7342c160ce74
Рублев Даниил Анатольевич <d.a.rublev@tinkoff.ru>
2024-03-25 22:19:59 +0300

    fix: any лучше чем ошибка))

--- client/src/node/grpc/utils.ts
+++ client/src/node/grpc/utils.ts
@@ -5,6 +5,7 @@ type UnpackedCallback<T> = T extends (err: infer E, result: infer U) => void
     ? U
     : T;
 type GenericFunction<TS extends any[]> = (...args: TS) => unknown;
+
 type Promisify<T> = {
     [K in keyof T]: T[K] extends GenericFunction<infer TS>
     ? (request: TS[0]) => Promise<UnpackedCallback<TS[3]>>
@@ -17,13 +18,13 @@ export const createService = <T extends Client>(
     const service = new ClientClass(DEFAULT_ADDRESS, DEFAULT_CREDS)
     const promisified: Promisify<T> = {} as any;
     for (const v in service) {
-        promisified[v] = (request: any) =>
+        promisified[v as keyof T] = ((request: unknown) =>
             new Promise((resolve, reject) => {
-                service[v](request, (err: Error, result: any) => {
+                (service[v as keyof Client] as any)(request, (err: Error, result: any) => {
                     if (err) return reject(err);
                     resolve(result);
                 });
-            });
+            })) as any;
     }
     return promisified;
 };
