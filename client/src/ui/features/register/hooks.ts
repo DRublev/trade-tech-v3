7abc1b5ef9b3e48b7f716c3a7d32f161602b8b86
Ivan Alekseev <iv.s.alekseev@tinkoff.ru>
2024-04-03 13:59:27 +0300

    добавил стор, перенес часть данных в слайсы

--- client/src/ui/features/register/hooks.ts
+++ client/src/ui/features/register/hooks.ts
@@ -1,13 +1,13 @@
-import { useIpcInoke } from "../../hooks";
+import { useIpcInvoke } from '../../hooks';
 import { useCallback } from 'react';
 import { useAppDispatch } from '../../../store';
 import { setRegisterData } from './registerSlice';
 
 
 
-export const useRegister = <I, O>() => useIpcInoke<I, O>("REGISTER");
-export const useSetAccount = <I, O>() => useIpcInoke<I, O>("SET_ACCOUNT");
-export const useGetAccount = <I, O>() => useIpcInoke<I, O>("GET_ACCOUNTS");
+export const useRegister = <I, O>() => useIpcInvoke<I, O>("REGISTER");
+export const useSetAccount = <I, O>() => useIpcInvoke<I, O>("SET_ACCOUNT");
+export const useGetAccount = <I, O>() => useIpcInvoke<I, O>("GET_ACCOUNTS");
 
 export const useRegistration = () => {
     const register = useRegister();
