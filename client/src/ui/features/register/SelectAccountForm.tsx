3e5a9e39a243d31969faf402dd9b7025cd8be90a
Рублев Даниил Анатольевич <d.a.rublev@tinkoff.ru>
2024-04-22 20:51:02 +0300

    fix: workflow change os

--- client/src/ui/features/register/SelectAccountForm.tsx
+++ client/src/ui/features/register/SelectAccountForm.tsx
@@ -134,7 +134,7 @@ export const SelectAccountForm = () => {
                             <Form.Submit asChild>
                                 <Button className={s.submitBtn}>Дальше</Button>
                             </Form.Submit>
-                            <Button onClick={onLogout}>
+                            <Button onClick={onLogout} color="crimson">
                               Выйти
                             </Button>
                         </Flex>
