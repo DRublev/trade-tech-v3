103e0d749a9bafe475ff39d412a781cb19e5e421
Рублев Даниил Анатольевич <d.a.rublev@tinkoff.ru>
2024-05-08 23:02:16 +0300

    fix: Фикс с отображением роута логина из-за задержки сервера

--- client/src/node/ipcHandlers/auth.ts
+++ client/src/node/ipcHandlers/auth.ts
@@ -6,12 +6,36 @@ import { createLogger } from '../logger';
 
 const log = createLogger({ controller: 'auth' });
 
+const retry = <T extends (...args: any) => any>(action: T, { attempts = 3, delay = 100 }) => (...args: Parameters<T>) => {
+    let attempt = 1;
+
+    const r: () => ReturnType<typeof action> = async () => {
+        try {
+            const res = await action(args);
+            return res;
+        } catch (e) {
+            if (attempt > attempts) {
+                throw e;
+            }
+            await new Promise((resolve) => setTimeout(resolve, delay));
+            attempt += 1;
+            return r();
+        }
+    }
+
+    return r();
+}
+
+
+// Сервак не успевает запускаться, но к этому моменту мы уже пытаемся дернуть этот метод, поэтому нужен ретрай
+const hasTokenWithRetry = retry(authService.hasToken, {});
+
 ipcMain.handle(ipcEvents.GET_AUTH_INFO, async (e) => {
     try {
         log.info('GET_AUTH_INFO');
         const isSandbox = await storage.get('isSandbox');
 
-        const { HasToken } = await authService.hasToken({});
+        const { HasToken } = await hasTokenWithRetry({});
 
         return { isAuthorised: !!HasToken, isSandbox };
     } catch (err) {
