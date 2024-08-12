3056c94390fedf2674ba8082e0cec4521952d6f9
Рублев Даниил Анатольевич <d.a.rublev@tinkoff.ru>
2024-08-12 17:40:50 +0300

    chore

--- server/build.sh
+++ server/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 cd ../server
 
+mkdir ./resources/app/go-binaries
+chown -R $(whoami) ./resources/app/go-binaries
+
 GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-windows.dll -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-macos.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
-GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-linux.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
+# GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-linux.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
+
 # GOOS=windows GOARCH=amd64 go build -o ./resources/app/go-binaries/app-binary-windows.exe -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 # GOOS=darwin GOARCH=amd64 go build -o ./resources/app/go-binaries/app-binary-macos -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 # GOOS=linux GOARCH=amd64  go build -o ./resources/app/go-binaries/app-binary-linux -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
--- server/buildSo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ../server
-
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-windows.dll -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-macos.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
-GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-linux.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
