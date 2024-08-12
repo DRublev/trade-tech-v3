55b13bcaae3bf067c0df9d1e8bba04b11a942633
Рублев Даниил Анатольевич <d.a.rublev@tinkoff.ru>
2024-08-12 19:39:15 +0300

    chore

--- server/build.sh
+++ server/build.sh
@@ -5,7 +5,7 @@ mkdir -p ./resources/app/go-binaries
 chown -R $(whoami) ./resources/app/go-binaries
 
 GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-windows.dll -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-macos.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
+GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-macos.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 # GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-linux.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 
 # GOOS=windows GOARCH=amd64 go build -o ./resources/app/go-binaries/app-binary-windows.exe -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
