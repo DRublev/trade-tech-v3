9276eb091b960a57c4da7232ad70a42eb5c3a484
Рублев Даниил Анатольевич <d.a.rublev@tinkoff.ru>
2024-08-12 20:04:40 +0300

    chore

--- server/build.sh
+++ server/build.sh
@@ -4,7 +4,10 @@ cd ../server
 mkdir -p ./resources/app/go-binaries
 chown -R $(whoami) ./resources/app/go-binaries
 
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-windows.dll -buildmode=dll -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
+set GOOS="windows"
+set GOARCH="amd64"
+set CGO_ENABLED=1
+go build -o ./resources/app/go-binaries/app-binary-windows.dll -x -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-macos.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 # GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-linux.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 
