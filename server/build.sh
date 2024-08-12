7b8199cf70ed2a05f53cf4a520092e537f8441c9
Рублев Даниил Анатольевич <d.a.rublev@tinkoff.ru>
2024-08-12 21:34:13 +0300

    chore

--- server/build.sh
+++ server/build.sh
@@ -4,7 +4,7 @@ cd ../server
 mkdir -p ./resources/app/go-binaries
 chown -R $(whoami) ./resources/app/go-binaries
 
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC="x86_64-linux-gnu-gcc" CXX="x86_64-linux-gnu-g++" go build -o ./resources/app/go-binaries/app-binary-windows.dll -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 GOGCCFLAGS='' CC=gcc go build -o ./resources/app/go-binaries/app-binary-windows.dll -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-macos.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 # GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -o ./resources/app/go-binaries/app-binary-linux.so -buildmode=c-shared -ldflags "-X main.envFromBuild=PROD -X main.secretFromBuild=trade-tech-secret-for-encryption"
 
